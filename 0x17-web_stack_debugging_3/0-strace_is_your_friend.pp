e6409087ce7d83f6fd708c46c1185ea5c7725940
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2023-03-06 11:50:15 +0000

    error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixing the 500 error when a GET HTTP method is requested to Apache web server
 
-exec {'replace':
-	provider => shell,
-	command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+exec { 'fixed-phpp':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin';
 }
